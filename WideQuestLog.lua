00cecc512fc1e25ab3e9e14fdd19cc9e2b9c1986
Mark Eliasen <mark.eliasen@pm.me>
2023-08-27 13:33:37 +0100

    Initial fixes

--- WideQuestLog.lua
+++ WideQuestLog.lua
@@ -36,11 +36,10 @@ local function ElvSkinFrames()
 	S:AddCallback('Quest', LoadSkin)
 end
 
-
 local function SkinFrames()
     -- Widen the window, note that this size includes some pad on the right hand
     -- side after the scrollbars
-    QuestLogFrame:SetWidth(724);
+    QuestLogFrame:SetWidth(772);
     QuestLogFrame:SetHeight(513);
 
     -- Adjust quest log title text
@@ -52,6 +51,20 @@ local function SkinFrames()
     QuestLogDetailScrollFrame:ClearAllPoints();
     QuestLogDetailScrollFrame:SetPoint("TOPLEFT", QuestLogListScrollFrame, "TOPRIGHT", 41, 0);
     QuestLogDetailScrollFrame:SetHeight(362);
+
+    -- Relocate the close button
+    QuestLogFrameCloseButton:ClearAllPoints();
+    QuestLogFrameCloseButton:SetPoint("TOPRIGHT", QuestLogFrame, "TOPRIGHT", -78, -8);
+
+    -- Relocate the Exit button and its text
+    QuestFrameExitButton:ClearAllPoints();
+    QuestFrameExitButton:SetPoint("BOTTOMRIGHT", QuestLogFrame, "BOTTOMRIGHT", -90, 53);
+    QuestFrameExitButtonText:ClearAllPoints();
+    QuestFrameExitButtonText:SetPoint("CENTER", QuestFrameExitButton, "CENTER", 0, 0);
+
+    -- Relocate the quest count
+    QuestLogQuestCount:ClearAllPoints();
+    QuestLogQuestCount:SetPoint("TOPLEFT", QuestLogDetailScrollFrame, "TOPRIGHT", -55, 28);
 
     -- Relocate the 'no active quests' text
     QuestLogNoQuestsText:ClearAllPoints();
@@ -83,49 +96,99 @@ local function SkinFrames()
     local regions = { QuestLogFrame:GetRegions() }
 
     -- Slightly freakish offsets to align the images with the frame
-    local xOffsets = { Left = 3; Middle = 259; Right = 515; }
-    local yOffsets =  { Top = 0; Bot = -256; }
+    local xOffsets = { LEFT = 3; MIDDLE = 259; RIGHT = 515; }
+    local yOffsets =  { TOP = 0; BOT = -256; }
 
     local textures = {
-        TopLeft = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_TopLeft";
-        TopMiddle = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_TopMid";
-        TopRight = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_TopRight";
-
-        BotLeft = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_BotLeft";
-        BotMiddle = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_BotMid";
-        BotRight = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_BotRight";
+        TOPLEFT = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_TopLeft";
+        TOPRIGHT = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_TopRight";
+        BOTLEFT = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_BotLeft";
+        BOTRIGHT = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_BotRight";
+        -- these will be replaced
+        TOPMIDDLE = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_TopMid";
+        BOTMIDDLE = "Interface\\AddOns\\WideQuestLog\\Icons\\DW_BotMid";
     }
 
-    local PATTERN = "^Interface\\QuestFrame\\UI%-QuestLog%-(([A-Z][a-z]+)([A-Z][a-z]+))$";
+    local nativeTextures = {
+        [136804] = {
+            texturePath = "Interface\\QuestFrame\\UI-QUESTLOG-TOPLEFT";
+            xofs = "LEFT";
+            yofs = "TOP";
+            which = "TOPLEFT";
+        };
+        [136805] = {
+            texturePath = "Interface\\QuestFrame\\UI-QUESTLOG-TOPRIGHT";
+            xofs = "RIGHT";
+            yofs = "TOP";
+            which = "TOPRIGHT";
+        };
+        [136798] = {
+            texturePath = "Interface\\QuestFrame\\UI-QUESTLOG-BOTLEFT";
+            xofs = "LEFT";
+            yofs = "BOT";
+            which = "BOTLEFT";
+        };
+        [136799] = {
+            texturePath = "Interface\\QuestFrame\\UI-QUESTLOG-BOTRIGHT";
+            xofs = "RIGHT";
+            yofs = "BOT";
+            which = "BOTRIGHT";
+        };
+        TOPMIDDLE = {
+            xofs = "MIDDLE";
+            yofs = "TOP";
+        };
+        BOTMIDDLE = {
+            xofs = "MIDDLE";
+            yofs = "BOT";
+        };
+    }
+
+    local PATTERN = "^Interface\\QuestFrame\\UI-QUESTLOG-(([A-Z]{3})([A-Z]+))$";
     for _, region in ipairs(regions) do
         if (region:IsObjectType("Texture")) then
             local texturefile = region:GetTexture();
-            local which, yofs, xofs = texturefile:match(PATTERN);
-            xofs = xofs and xOffsets[xofs];
-            yofs = yofs and yOffsets[yofs];
-            if (xofs and yofs and textures[which]) then
-                region:ClearAllPoints();
-                region:SetPoint("TOPLEFT", QuestLogFrame, "TOPLEFT", xofs, yofs);
-                region:SetTexture(textures[which]);
-                region:SetWidth(256);
-                region:SetHeight(256);
-                textures[which] = nil;
+            local which, yofs, xofs;
+
+            if (type(texturefile) == 'string') then
+                which, yofs, xofs = texturefile:match(PATTERN);
+            else
+                -- failover to texture IDs
+                if (nativeTextures[texturefile]) then
+                    texturefile = nativeTextures[texturefile];
+                    which = texturefile.which;
+                    yofs = texturefile.yofs;
+                    xofs = texturefile.xofs;
+                end
+            end
+
+            if (which) then
+                xofs = xofs and xOffsets[xofs];
+                yofs = yofs and yOffsets[yofs];
+                if (xofs and yofs and textures[which]) then
+                    region:SetPoint("TOPLEFT", QuestLogFrame, "TOPLEFT", xofs, yofs);
+                    region:SetTexture(textures[which]);
+                    region:SetWidth(256);
+                    region:SetHeight(256);
+                    textures[which] = nil;
+                end
             end
         end
     end
 
     -- Add in the new ones
     for name, path in pairs(textures) do
-        local yofs, xofs = name:match("^([A-Z][a-z]+)([A-Z][a-z]+)$");
-        xofs = xofs and xOffsets[xofs];
-        yofs = yofs and yOffsets[yofs];
-        if (xofs and yofs) then
-            local region = QuestLogFrame:CreateTexture(nil, "ARTWORK");
-            region:ClearAllPoints();
-            region:SetPoint("TOPLEFT", QuestLogFrame, "TOPLEFT", xofs, yofs);
-            region:SetWidth(256);
-            region:SetHeight(256);
-            region:SetTexture(path);
+        if (path and nativeTextures[name]) then
+            local xofs = nativeTextures[name].xofs;
+            local yofs = nativeTextures[name].yofs;
+            if (xOffsets[xofs] and yOffsets[yofs]) then
+                local region = QuestLogFrame:CreateTexture(nil, "ARTWORK");
+                region:ClearAllPoints();
+                region:SetPoint("TOPLEFT", QuestLogFrame, "TOPLEFT", xOffsets[xofs], yOffsets[yofs]);
+                region:SetWidth(256);
+                region:SetHeight(256);
+                region:SetTexture(path);
+            end
         end
     end
 
